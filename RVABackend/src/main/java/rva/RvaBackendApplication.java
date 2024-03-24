cbe168d8a21ebb0377dc89e647cab06c02c6eda1
ljiljanadjuric <djuricljiljana34@gmail.com>
2024-03-24 17:01:37 +0100

    backend done

--- RVABackend/src/main/java/rva/RvaBackendApplication.java
+++ RVABackend/src/main/java/rva/RvaBackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//pocetno izvrsavanje programa oznacava anotacija
+
 public class RvaBackendApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class RvaBackendApplication {
 	}
 
 }
+
